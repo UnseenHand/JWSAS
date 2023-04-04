2203f47df0de866cf0e0e328ad4292e1d369320b
UnseenHand <shnobel12@gmail.com>
2023-04-04 16:42:37 +0300

    Restructuring Controller For Scanning The Website
    Changes:
    - name change for the controller to be more appropriate
    - provided a controller with service injection for dividing the responsibilities
    - creating the /scanner/'controllerMethod' URL path for the scanner controller to border it off the other controllers

--- src/main/java/com/vuvp/wsas/controllers/ScannerController.java
+++ src/main/java/com/vuvp/wsas/controllers/ScannerController.java
@@ -1,45 +1,34 @@
 package com.vuvp.wsas.controllers;
 
+import com.vuvp.wsas.services.ResponseTimeService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/scanner")
 public class ScannerController {
 
-    //SQL Injection
-    //HTTPS/HTTP
-    //Response Time
-    @GetMapping("/scan")
-    @ResponseBody
-    public static long scan(String url) throws URISyntaxException, IOException, InterruptedException {
-        System.out.println(url);
+    private final ResponseTimeService responseTimeService;
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
-                .uri(URI.create(url))
-                .headers("Accept-Enconding", "gzip, deflate")
-                .build();
-//        HttpResponse<String> response =
-        var startTime = System.currentTimeMillis();
-
-        client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        var endTime = System.currentTimeMillis();
-
-        var timeTaken = endTime - startTime;
-        System.out.println("Response time:" + timeTaken + "ms");
-        return timeTaken;
-/*        String responseBody = response.body();
-        int responseStatusCode = response.statusCode();
-
-
-        System.out.println("httpGetRequest: " + responseBody);
-        System.out.println("httpGetRequest status code: " + responseStatusCode);*/
+    public ScannerController(ResponseTimeService responseTimeService) {
+        this.responseTimeService = responseTimeService;
     }
+
+    //Get Response Time
+    //Send a simple Get Request by the url parameter and measure the response time.
+    @GetMapping("/scanForGetResponseTime")
+    @ResponseBody
+    public long scanForGetResponseTime(String url) {
+        return responseTimeService.getResponseTime(url);
+    }
+
+
+    //SQL Injection
+
+
+    //HTTPS/HTTP
+
+
 }
