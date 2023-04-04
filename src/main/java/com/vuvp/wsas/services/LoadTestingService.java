84163c2cd35082335c9e1416f54bd88690a8862b
UnseenHand <shnobel12@gmail.com>
2023-04-05 00:18:14 +0300

    Fixing Some Config Issues Between Packages
    Changes:
    - fixed 'slf4j-api' library compatibility issue that caused Runtime errors
    - added main service class for managing interaction with JMeter API

---
+++ src/main/java/com/vuvp/wsas/services/LoadTestingService.java
@@ -0,0 +1,7 @@
+package com.vuvp.wsas.services;
+
+
+//This service should be the gate (entrance point) or the main
+// service to the interaction between JMeter API and my project
+public class LoadTestingService {
+}
